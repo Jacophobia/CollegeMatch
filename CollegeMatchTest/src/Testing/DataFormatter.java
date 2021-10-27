e7bf22dbb5df172b7ed1b225b782afb19b41dec3
Jacophobia <jacophilia@gmail.com>
2021-10-27 13:42:46 -0600

    Trying it again

---
+++ CollegeMatchTest/src/Testing/DataFormatter.java
@@ -0,0 +1,37 @@
+//package Testing;
+//
+//import java.util.ArrayList;
+//
+//public class DataFormatter {
+//    ArrayList<College> colleges;
+//    DataFormatter(ArrayList<College> colleges) {
+//        this.colleges = colleges;
+//    }
+//
+//    public String getNextCollegeName() {
+//        return null;
+//    }
+//    public float getNextCollegeGPA() {
+//        return null;
+//    }
+//    public int getNextCollegeACT() {
+//        return null;
+//    }
+//    public int getNextCollegeSAT() {
+//        return null;
+//    }
+//    public ArrayList<String> getNextCollegeMajors() {
+//        return null;
+//    }
+//    public double getNextCollegePPS() { // price per semester
+//        return null;
+//    }
+//    public String getNextCollegeLocation() {
+//        return null;
+//    }
+//
+//    public int size() {
+//        return this.colleges.size();
+//    }
+//
+//}
---
+++ CollegeMatchTest/src/Testing/DataRetriever.java
@@ -0,0 +1,15 @@
+package Testing;
+
+import java.util.ArrayList;
+
+public class DataRetriever {
+    public DataRetriever() {
+
+    }
+
+    public ArrayList<College> getCollegeList() {
+        return null;
+    }
+
+
+}
